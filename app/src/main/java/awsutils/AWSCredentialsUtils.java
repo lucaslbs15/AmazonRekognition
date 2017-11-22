031cae6ce5d40555b0112ab05d39fff162e03f59
Lucas Bicca <lucas.bicca@ilegra.com>
2017-11-22 08:39:12 -0200

    Removing Amazon public key from code

--- app/src/main/java/awsutils/AWSCredentialsUtils.java
+++ app/src/main/java/awsutils/AWSCredentialsUtils.java
@@ -7,12 +7,13 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 
+import lucas.bicca.amazonrekognition.R;
+
 public class AWSCredentialsUtils {
 
-    public static AWSCredentials createCredentials() {
+    public static AWSCredentials createCredentials(Context context) {
         try {
-            //return new ProfileCredentialsProvider("AdminUser").getCredentials();
-            return new BasicAWSCredentials("AKIAI3F6E73ZMK2VQ56A", "3qoIsx/bLSOBB2NhCxXt+9FF553j90yXbhrcncLa");
+            return new BasicAWSCredentials(context.getString(R.string.aws_access_key), context.getString(R.string.aws_secret_key));
         } catch (Exception ex) {
             throw new AmazonClientException("Cannot load the credentials from the credential " +
                     "profiles file. Please make sure that your credentials file is at the, and is in valid format.", ex);
